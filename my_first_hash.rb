26676ed4d2f816b82c71704ab0fced7eb298ced3
Natalie Taktachev <ntaktach@gmail.com>
2019-07-21 21:35:02 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,9 +1,8 @@
 def my_hash
-  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
-  
-end
-
+  snowboard = {
+    "brand" => "Salomon"
+  }
+end 
 
 def shipping_manifest
   # set a variable called `the_manifest`, equal to a hash
